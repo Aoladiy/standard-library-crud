3507296790580ea09afbe85fe296bbcc39bf27b3
Ochirov Aldar <ochirov-3@mail.ru>
2026-01-04 16:36:44 +0300

    - added simple logger middleware and logic of registration of middleware

--- router/router.go
+++ router/router.go
@@ -1,17 +1,66 @@
 package router
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/Aoladiy/standard-library-crud/item"
 )
 
+type CustomResponseWriter struct {
+	http.ResponseWriter
+	StatusCode int
+}
+
+func (w *CustomResponseWriter) WriteHeader(statusCode int) {
+	w.StatusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
 func SetupRouter() http.Handler {
 	router := http.NewServeMux()
-	router.HandleFunc("GET /item/{id}", item.GetItemHandler)
+	router.Handle("GET /item/{id}", SecondLoggerMiddleware(LoggerMiddleware(http.HandlerFunc(item.GetItemHandler))))
 	router.HandleFunc("GET /item", item.GetItemsHandler)
 	router.HandleFunc("POST /item", item.CreateItemHandler)
 	router.HandleFunc("PATCH /item/{id}", item.UpdateItemHandler)
 	router.HandleFunc("DELETE /item/{id}", item.DeleteItemHandler)
-	return router
+	return RegisterMiddleware(
+		router,
+		LoggerMiddleware,
+		SecondLoggerMiddleware,
+	)
+}
+
+func LoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cw := &CustomResponseWriter{
+			ResponseWriter: w,
+			StatusCode:     http.StatusOK,
+		}
+		start := time.Now()
+		next.ServeHTTP(cw, r)
+		finish := time.Since(start)
+		log.Println(cw.StatusCode, r.Method, r.URL.Path, finish)
+	})
+}
+
+func SecondLoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cw := &CustomResponseWriter{
+			ResponseWriter: w,
+			StatusCode:     http.StatusOK,
+		}
+		start := time.Now()
+		next.ServeHTTP(cw, r)
+		finish := time.Since(start)
+		log.Println(cw.StatusCode, r.Method, r.URL.Path, finish, "PAY ATTENTION!!! This is second middleware!!!")
+	})
+}
+
+func RegisterMiddleware(handler http.Handler, middleware ...func(next http.Handler) http.Handler) http.Handler {
+	for i := len(middleware) - 1; i >= 0; i-- {
+		handler = middleware[i](handler)
+	}
+	return handler
 }
