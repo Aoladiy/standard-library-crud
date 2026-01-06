e4cbf6f04f00733bf67fb0a1898da41f466b39c6
Ochirov Aldar <ochirov-3@mail.ru>
2026-01-06 12:56:24 +0300

    - added graceful shutdown

--- cmd/main/server.go
+++ cmd/main/server.go
@@ -1,36 +1,45 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Aoladiy/standard-library-crud/internal/envLoading"
 )
 
 func setupServer(handler http.Handler) {
 	server := &http.Server{
-		Addr:                         envLoading.EnvVars.Addr,
-		Handler:                      handler,
-		DisableGeneralOptionsHandler: false,
-		TLSConfig:                    nil,
-		ReadTimeout:                  envLoading.EnvVars.ReadTimeout,
-		ReadHeaderTimeout:            envLoading.EnvVars.ReadHeaderTimeout,
-		WriteTimeout:                 envLoading.EnvVars.WriteTimeout,
-		IdleTimeout:                  envLoading.EnvVars.IdleTimeout,
-		MaxHeaderBytes:               0,
-		TLSNextProto:                 nil,
-		ConnState:                    nil,
-		ErrorLog:                     nil,
-		BaseContext:                  nil,
-		ConnContext:                  nil,
-		HTTP2:                        nil,
-		Protocols:                    nil,
+		Addr:              envLoading.EnvVars.Addr,
+		Handler:           handler,
+		ReadTimeout:       envLoading.EnvVars.ReadTimeout,
+		ReadHeaderTimeout: envLoading.EnvVars.ReadHeaderTimeout,
+		WriteTimeout:      envLoading.EnvVars.WriteTimeout,
+		IdleTimeout:       envLoading.EnvVars.IdleTimeout,
 	}
 
 	log.Printf("server listening %v started\n", envLoading.EnvVars.Addr)
-	err := server.ListenAndServe()
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Println(err)
+			return
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(5)*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println("Error while shutdown", err)
 		return
 	}
+	log.Println("Graceful shutdown complete")
 }
